76893f45d9c08cfb8318669f2a0df550085c889b
iPAS <ptiwatthanont@gmail.com>
2020-06-17 19:30:13 +0700

    Use `mbed add http://os.mbed.com/users/nkhorman/code/Adafruit_GFX/` instead, and use with I2C super class.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS     1000
+DigitalOut led(LED3);  // Initialise the digital pin LED1 as an output
 
 
 // SSD1306 adapted from Adafruit's library
@@ -22,21 +23,27 @@ class I2CPreInit : public I2C  // an I2C sub-class that provides a constructed d
     };
 };
 
-I2CPreInit           gI2C(I2C_SDA, I2C_SCL);
-Adafruit_SSD1306_I2c gOled2(gI2C, P_9, SSD_I2C_ADDRESS, 64, 128);
+//I2CPreInit
+I2C           gI2C(I2C_SDA, I2C_SCL);
+Adafruit_SSD1306_I2c gOled2(gI2C, P_5, SSD_I2C_ADDRESS, 64, 128);
 
 
 int main()
 {
-    DigitalOut led(LED3);  // Initialise the digital pin LED1 as an output
+    uint16_t x = 0;
 
+    gOled2.clearDisplay();
     gOled2.printf("%ux%u OLED Display\r\n", gOled2.width(), gOled2.height());
-    // gOled2.display();
-    // gOled2.clearDisplay();
+    gOled2.display();
+    ThisThread::sleep_for(5000);
 
     while (true) {
         led = !led;
         // led = 1;
         ThisThread::sleep_for(BLINKING_RATE_MS);
+
+        gOled2.clearDisplay();
+        gOled2.printf("%u\r", x++);
+        gOled2.display();
     }
 }
