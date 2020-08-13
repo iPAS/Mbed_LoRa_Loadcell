d3c16f9c15fc34bf54b17e6f9f837b5ffad336da
iPAS <ptiwatthanont@gmail.com>
2020-08-13 13:25:09 +0700

    Complete interfacing on HX711 and ADS1232, yet truely understanding.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "mbed.h"
 #include "Adafruit_SSD1306.h"
 #include "Hx711.h"
+#include "ADS1231.h"
 
 
 // Blinking rate in milliseconds
@@ -25,7 +26,7 @@ class I2CPreInit : public I2C  // an I2C sub-class that provides a constructed d
 };
 
 //I2CPreInit
-I2C           gI2C(I2C_SDA, I2C_SCL);
+I2C                  gI2C(I2C_SDA, I2C_SCL);
 Adafruit_SSD1306_I2c gOled2(gI2C, P_5, SSD_I2C_ADDRESS, 64, 128);
 
 
@@ -34,14 +35,91 @@ Adafruit_SSD1306_I2c gOled2(gI2C, P_5, SSD_I2C_ADDRESS, 64, 128);
 Serial uart(UART_TX, UART_RX, NULL, 115200);
 
 
-// https://os.mbed.com/users/megrootens/code/HX711/
-// https://os.mbed.com/users/jmiller322/code/SmartCrutches//file/d5e36ee82984/main.cpp/
-// https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide
-// https://www.thaieasyelec.com/article-wiki/review-product-article/how-to-use-load-cell-and-hx711-amplifier-module.html
-// https://www.thaieasyelec.com/media/wysiwyg/blog/30.png
-Hx711 loadcell(P_8, P_9, 0, 0.005, 128);
+/******************************************************************************
+ * HX711
+ * 
+ * https://os.mbed.com/users/megrootens/code/HX711/
+ * https://os.mbed.com/users/jmiller322/code/SmartCrutches//file/d5e36ee82984/main.cpp/
+ * https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide
+ * https://www.thaieasyelec.com/article-wiki/review-product-article/how-to-use-load-cell-and-hx711-amplifier-module.html
+ * https://www.thaieasyelec.com/media/wysiwyg/blog/30.png
+ ******************************************************************************/
+Hx711 loadcell_hx711(P_8, P_9, 0, 0.005, 128);  // Hx711(PinName pin_sck, PinName pin_dt, int offset, float scale, uint8_t gain = 128)
+Ticker hx711_ticker;
+struct 
+{
+    uint32_t data;
+    float mass;
+} hx711_sample;
+
+void hx711_read(void)
+{
+    hx711_sample.data = loadcell_hx711.readRaw();
+    hx711_sample.mass = loadcell_hx711.read();
+}
+
+void hx711_init(void)
+{
+    // loadcell_hx711.set_offset(124);
+    hx711_ticker.attach(&hx711_read, 1);
+}
 
 
+/******************************************************************************
+ * ADS1232
+ *
+ * https://os.mbed.com/users/mcm/code/ADS1231/
+ ******************************************************************************/
+ADS1231  loadcell_ads1232(P_25, P_29);
+Ticker ads1232_ticker;
+struct 
+{
+    ADS1231::ADS1231_status_t status;
+    ADS1231::Vector_count_t   data;
+    ADS1231::Vector_mass_t    calculated_mass;
+    ADS1231::Vector_voltage_t calculated_volt;
+} ads1232_sample;
+
+void ads1232_read(void) {
+    ads1232_sample.status          = loadcell_ads1232.ADS1231_ReadRawData(&ads1232_sample.data, 4);
+    ads1232_sample.calculated_mass = loadcell_ads1232.ADS1231_CalculateMass(&ads1232_sample.data, 1.0, ADS1231::ADS1231_SCALE_kg);
+    ads1232_sample.calculated_volt = loadcell_ads1232.ADS1231_CalculateVoltage(&ads1232_sample.data, 5.0);
+}
+
+void ads1232_init(void)
+{
+    ADS1231::ADS1231_status_t sts;
+
+    // Reset and wake the ADS1232 up
+    sts = loadcell_ads1232.ADS1231_PowerDown();
+    if (sts == ADS1231::ADS1231_status_t::ADS1231_FAILURE)
+    {
+        uart.printf("ADS1232 fail on power-down\r\n");
+    }
+    
+    sts = loadcell_ads1232.ADS1231_Reset();
+    if (sts == ADS1231::ADS1231_status_t::ADS1231_FAILURE)
+    {
+        uart.printf("ADS1232 fail on reset\r\n");
+    }
+    wait(1);
+ 
+    /** CALIBRATION time start!  **/
+    // 1. REMOVE THE MASS ON THE LOAD CELL ( ALL LEDs OFF ). Read data without any mass on the load cell
+    // aux = myWeightSensor.ADS1231_ReadData_WithoutMass ( &myData, 4 );
+    // 2. PUT A KNOWN MASS ON THE LOAD CELL ( JUST LED1 ON ). Read data with an user-specified calibration mass
+    // aux = myWeightSensor.ADS1231_ReadData_WithCalibratedMass ( &myData, 4 );
+
+    // Calculating the tare weight ( JUST LED3 ON )
+    // aux = myWeightSensor.ADS1231_SetAutoTare ( ADS1231::ADS1231_SCALE_kg, &myData, 5 );
+
+    ads1232_ticker.attach(&ads1232_read, 1);  // the address of the function to be attached ( readDATA ) and the interval ( 0.5s ) ( JUST LED4 BLINKING )
+}
+
+
+/******************************************************************************
+ * Main 
+ ******************************************************************************/
 int main()
 {
     uint16_t x = 0;
@@ -49,20 +127,36 @@ int main()
     gOled2.clearDisplay();
     gOled2.printf("%ux%u OLED Display\r\n", gOled2.width(), gOled2.height());
     gOled2.display();
-    ThisThread::sleep_for(5000);
 
+    hx711_init();
+    ads1232_init();
+
+    // wait(3);
     while (true) {
         led = !led;
         // led = 1;
         ThisThread::sleep_for(BLINKING_RATE_MS);
 
-        float f = loadcell.read();
 
         gOled2.clearDisplay();
-        gOled2.printf("%u: %.2f\r", x, f);
+        gOled2.printf("%u\r", x);
         gOled2.display();
 
-        uart.printf("%u: %.2f\r\n", x, f);
+
+        uart.printf("HX711: raw=%ld mass=%.2f\r\n", hx711_sample.data, hx711_sample.mass);
+
+
+        if (ads1232_sample.status == ADS1231::ADS1231_status_t::ADS1231_FAILURE)
+            uart.printf("ADS1232 fail on readRaw()\r\n");
+        else
+            uart.printf("ADS1232: raw=%ld  mass=%0.5fkg voltage=%0.5fmV\r\n",
+                (uint32_t)ads1232_sample.data.myRawValue,
+                ads1232_sample.calculated_mass.myMass, 
+                ads1232_sample.calculated_volt.myVoltage * 1000);
+
+
+        
+
 
         x++;
     }
