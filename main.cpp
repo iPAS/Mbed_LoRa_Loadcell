33307b11a9f38b60303f8903efbb897d0c7699ba
iPAS <ptiwatthanont@gmail.com>
2021-10-30 17:19:41 +0700

    Solved the multi-thread stack problem.

--- main.cpp
+++ main.cpp
@@ -327,16 +327,37 @@ void ads1220_read(void)
 
 
 /******************************************************************************
+ * AUX functions
+ ******************************************************************************/
+void print_memory_info() {
+    // allocate enough room for every thread's stack statistics
+    int cnt = osThreadGetCount();
+    mbed_stats_stack_t *stats = (mbed_stats_stack_t*) malloc(cnt * sizeof(mbed_stats_stack_t));
+ 
+    cnt = mbed_stats_stack_get_each(stats, cnt);
+    for (int i = 0; i < cnt; i++) {
+        tr_debug("Thread: 0x%lX, Stack size: %lu / %lu\r\n", stats[i].thread_id, stats[i].max_size, stats[i].reserved_size);
+    }
+    free(stats);
+ 
+    // Grab the heap statistics
+    mbed_stats_heap_t heap_stats;
+    mbed_stats_heap_get(&heap_stats);
+    tr_debug("Heap size: %lu / %lu bytes\r\n", heap_stats.current_size, heap_stats.reserved_size);
+}
+
+
+/******************************************************************************
  * Main
  ******************************************************************************/
 int main()
 {
     // Setup tracing
     setup_trace();
-
+    print_memory_info();
     // Initialize lorawan & sending
     lrw_init();
-
+    print_memory_info();
 
     float raw = 0;
 
